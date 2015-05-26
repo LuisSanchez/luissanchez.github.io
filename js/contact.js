c9b0d7dcbd9fb105081733c80c22d951c0b43e78
Luis Sanchez <luis.sanchezm86@gmail.com>
2015-05-26 16:49:52 -0300

    New content
    - Adding infography from ux pin in a new page.
    - Updating footer.
    - Adding map on contacts.
    - Updating version of bootstrap.

--- js/contact.js
+++ js/contact.js
@@ -1,10 +1,31 @@
+var map;
+var marker;
+var latLng = new google.maps.LatLng(-33.4735291, -70.6449825)
+
+function initialize() {
+	var mapOptions = {
+	    zoom: 13,
+	    center: latLng,
+	    scrollwheel: false
+	};
+
+	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+	marker = new google.maps.Marker({
+	    map: map,
+	    draggable: true,
+	    animation: google.maps.Animation.DROP,
+	    position: latLng
+	});
+}
+
 $(document).ready(function($) {
 	// init controller
 	var controller = new ScrollMagic({globalSceneOptions: {triggerHook: "onEnter", duration: $(window).height()*2}});
 
 	// build scenes
 	new ScrollScene({triggerElement: "#parallax1"})
-				.setTween(TweenMax.from("#parallax1 > div", 1, {top: "-80%", ease: Linear.easeNone}))
+				.setTween(TweenMax.from("#parallax1 > div", 1, {top: "-50%", ease: Linear.easeNone}))
 				.addTo(controller);
 				// .addIndicators({zindex: 1, suffix: "1"});
 	
@@ -27,4 +48,6 @@ $(document).ready(function($) {
 		.triggerElement("#ts-2")
 		.setTween(TweenMax.from("#ts-2", 1, {autoAlpha: 0}));
 		// .addIndicators({zindex: 3, suffix: "3"});
+		
+	google.maps.event.addDomListener(window, 'load', initialize);
 });
